0ceb748cceec5d774b9017e545b2c8fdc20a4164
Nabeel Hasnain <hasnainabeel@gmail.com>
2022-05-30 14:05:14 +0530

    feat: linked server

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 var translateButton = document.querySelector("#translate-btn")
 var inputText = document.querySelector("#inputText")
 var outputDiv = document.querySelector(".output")
+var URL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 translateButton.addEventListener("click",clickEventHandler)
 
 
 function clickEventHandler(){
     console.log("Clicked")
-    console.log("input is", inputText.value)
     outputDiv.innerText = inputText.value
-
+    fetch(makingURL())
+    .then(response => response.json())
+    .then(json => console.log(json))
 }
 
+function makingURL() {
+    var finalURL = (URL + "?text=" + inputText.value);
+    return finalURL
+}
