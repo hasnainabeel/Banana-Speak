528155d167676efdef03fc382cb9371a8b11b3fa
Nabeel Hasnain <hasnainabeel@gmail.com>
2022-05-30 10:50:07 +0530

    docs: reads text
    reads text from textbox if the button is clicked and is displayed in console

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-var textSelector = document.querySelector();
+var translateButton = document.querySelector("#translate-btn")
+var inputText = document.querySelector("#inputText")
+
+
+translateButton.addEventListener("click",clickEventHandler)
+function clickEventHandler(){
+    console.log("Clicked")
+    console.log("input is", inputText.value)
+}
+
+/* 
+    1."textarea"
+    2.".btn-primary"
+    3."#input-btn"
+    4."input[name='translator']"
+*/
